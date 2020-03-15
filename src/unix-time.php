49109b8183ab27e38404c7cd3e085a360ce8e756
Terary Chambers <terary@gmail.com>
2020-03-15 04:18:15 -0700

    initialize repo

---
+++ src/unix-time.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace learning\awesomeo; // see "Defining Namespaces" section
+
+class aw_time  {
+
+    function unix(){
+        return time();
+    }
+}
+
+// function aw_time() {
+//     return time();
+// }
